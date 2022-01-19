44be8ef29e72e7fb6f8147644b65f5b22fb5cb00
Harish <warriorharish95668@gmail.com>
2022-01-19 17:18:55 +0530

    Time: 0 ms (100.00%), Space: 39 MB (68.57%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        
+        List<Boolean> result=new ArrayList<Boolean>();
+        int largest = 0;
+        for(int candie: candies){
+            if(candie>largest){
+                largest=candie;
+            }
+        }
+        
+        for(int candie:candies){
+            if((candie+extraCandies)>largest ||(candie+extraCandies) == largest){
+                result.add(true);
+            }else{
+                result.add(false);
+            }
+        }
+        return result;
+    } 
+    
+}
