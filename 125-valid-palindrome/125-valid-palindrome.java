8d1783470b3f4cc0fafed77f04b9e1f3110aed39
Harish <warriorharish95668@gmail.com>
2022-05-16 22:35:04 +0530

    Time: 4 ms (81.03%), Space: 43.7 MB (54.26%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+     
+        if(s.isEmpty()){
+            return false;
+        }
+        int head=0;
+        int tail= s.length()-1;
+        
+        while(head<tail){
+            char headChar=s.charAt(head);
+            char tailChar=s.charAt(tail);
+            if(!Character.isLetterOrDigit(headChar)){
+                head++;
+            }else if(!Character.isLetterOrDigit(tailChar)){
+                tail--;
+            }else{
+                if(Character.toLowerCase(headChar)!=Character.toLowerCase(tailChar)){
+                    return false;
+                }
+                head++;
+                tail--;
+            }
+        }return true;
+    }
+}
